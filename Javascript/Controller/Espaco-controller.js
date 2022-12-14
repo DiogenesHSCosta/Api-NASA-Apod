9bc55027aa907c3f316e27a6536ce4fd0de585cc
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2022-12-14 18:34:54 -0300

    first commit

---
+++ Javascript/Controller/Espaco-controller.js
@@ -0,0 +1,44 @@
+const descricao = document.querySelector("#descricao")
+const dataInput = document.querySelector('#data')
+const fundo = document.querySelector("#fundo")
+
+//dia, ano e mes atual
+    let data = new Date();
+    let anoAtual = data.getFullYear()
+    let mesAtual = data.getMonth() + 1
+    let diaAtual = data.getDate()
+
+    dataInput.setAttribute("max", `${anoAtual}-${mesAtual}-${diaAtual}`)
+
+
+dataInput.addEventListener("change", async () =>{
+    console.log(dataInput.value)
+   //fetch
+
+    const query = `&date=${dataInput.value}`
+    const api = await fetch(`https://api.nasa.gov/planetary/apod?api_key=MUBfu3uljTbDvHs4icZc1m6qoFMddOcquh6rwO8G${query}`)
+    let dado = await api.json()
+    console.log(dado)
+
+    var espaco = new Espaco(dado)
+    console.log(espaco)
+   
+    fundo.setAttribute("src", espaco.imagem)
+    let titulo = espaco.titulo
+    //informação da descrição
+    descricao.innerHTML = `<div class='divisao'> <h1 class='titulo'>${titulo}</h1> </div> <div class='divisao'><p class='texto'>${espaco.descricao}</p></div>`
+
+    
+})
+
+document.getElementsByTagName('figure')[0].addEventListener("click", () =>{
+    
+    if(descricao.style.opacity == 0){
+        descricao.style.opacity = "0.7"
+    }
+    
+})
+
+descricao.addEventListener("click", () =>{
+    descricao.style.opacity = "0"
+})
---
+++ Javascript/Model/Espaco-model.js
@@ -0,0 +1,8 @@
+class Espaco{
+  
+    constructor(espaco){
+        this.titulo = espaco.title
+        this.descricao = espaco.explanation
+        this.imagem = espaco.url
+    }
+}
